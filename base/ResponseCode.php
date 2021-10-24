f594e2a4a0fc2be2e9e0c9e56c8ec4e9dbdf6307
mohammadnejad <mohammadnejad.job@gmail.com>
2021-10-24 13:51:03 +0330

    tutfw

---
+++ base/ResponseCode.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace tutfw\vendor\tutfw\base;
+
+
+class ResponseCode
+{
+	const E100 = 100; // Continue
+	const E101 = 101; // Switching Protocols
+	const E200 = 200; // OK
+	const E201 = 201; // Created
+	const E202 = 202; // Accepted
+	const E203 = 203; // Non-Authoritative Information
+	const E204 = 204; // No Content
+	const E205 = 205; // Reset Content
+	const E206 = 206; // Partial Content
+	const E300 = 300; // Multiple Choices
+	const E301 = 301; // Moved Permanently
+	const E302 = 302; // Moved Temporarily
+	const E303 = 303; // See Other
+	const E304 = 304; // Not Modified
+	const E305 = 305; // Use Proxy
+	const E400 = 400; // Bad Request
+	const E401 = 401; // Unauthorized
+	const E402 = 402; // Payment Required
+	const E403 = 403; // Forbidden
+	const E404 = 404; // Not Found
+	const E405 = 405; // Method Not Allowed
+	const E406 = 406; // Not Acceptable
+	const E407 = 407; // Proxy Authentication Required
+	const E408 = 408; // Request Time-out
+	const E409 = 409; // Conflict
+	const E410 = 410; // Gone
+	const E411 = 411; // Length Required
+	const E412 = 412; // Precondition Failed
+	const E413 = 413; // Request Entity Too Large
+	const E414 = 414; // Request-URI Too Large
+	const E415 = 415; // Unsupported Media Type
+	const E500 = 500; // Internal Server Error
+	const E501 = 501; // Not Implemented
+	const E502 = 502; // Bad Gateway
+	const E503 = 503; // Service Unavailable
+	const E504 = 504; // Gateway Time-out
+	const E505 = 505; // HTTP Version not supported
+}
---
+++ lang/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'not_found' => 'Not found page',
+	'bad_request' => 'Bad request',
+	'bad_request_route' => 'Bad request route',
+	'not_found_controller' => 'Not found controller',
+	'not_found_action' => 'Not found action',
+];
