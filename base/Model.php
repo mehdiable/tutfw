2980e97e77f46cb8e9b87b05abac1202c2414822
mehdi mn <mohammadnejad.job@gmail.com>
2025-10-12 17:59:53 +0330

    update Model

--- base/Model.php
+++ base/Model.php
@@ -2,34 +2,63 @@
 
 namespace tutfw\base;
 
-use tutfw\db\DBDriverFactory;
-use tutfw\db\drivers\MongoDB;
-use tutfw\db\drivers\MySQL;
-use tutfw\db\ExceptionDBDriver;
-use tutfw\interfaces\IModel;
+use MongoDB\Driver\Cursor;
+use MongoDB\Model\BSONDocument;
 use tutfw\TutFw;
 
 /**
  * Class Model
  *
+ * @property string $database
+ * @property string $collection
+ * @property string $user
+ * @property string $password
+ * @property string $host
+ * @property string $port
+ * @property string $uri
+ * @property \MongoDB\Client $client
  *
- * @package tutfw\base
+ * @package tutfw\vendor\tutfw\base
  */
-class Model
+class Model extends BaseObject
 {
-	protected string $driver = DBDriverFactory::DEFAULT_DRIVER;
-	protected IModel|null $driverObj = null;
-	protected string $connection = 'default';
+	protected $connection = 'default';
+	protected $authDatabase= null;
+	protected $database = null;
+	protected $collection = null;
+	protected $user = null;
+	protected $password = null;
+	protected $host = null;
+	protected $port = null;
+	private $uri = 'mongodb://';
+	protected $client = null;
+	protected $dateKeys = [];
+	/** @var array|null|object */
+	protected $result = null;
+	public array $dynamicProperty = [];
 
-	/**
-	 * @throws ExceptionDBDriver
-	 */
-	public function __construct(?array $db = null)
+	public function __construct()
 	{
-		if (!$db) {
-			$db = include TutFw::getRootPath() . '/conf/db.php';
+		$db = include TutFw::getRootPath() . '/conf/db.php';
+		parent::__construct($db[$this->connection]);
+	}
+
+	public function &__get(string $name)
+	{
+		if (!isset($this->dynamicProperty[$name])) {
+			$this->dynamicProperty[$name] = null;
 		}
-		$this->driver($db[$this->connection] ?? []);
+		return $this->dynamicProperty[$name];
+	}
+
+	public function __set(string $name, $value): void
+	{
+		$this->dynamicProperty[$name] = $value;
+	}
+
+	public function __isset(string $name): bool
+	{
+		return isset($this->dynamicProperty[$name]);
 	}
 
 	/**
@@ -37,37 +66,275 @@ class Model
 	 *
 	 * @return static
 	 */
-	public static function ins(): static
+	public static function ins()
 	{
 		return new static();
 	}
 
-	/**
-	 * @param array $params
-	 *
-	 * @return IModel|MongoDB|MySQL
-	 * @throws ExceptionDBDriver
-	 */
-	public function driver(array $params = []): IModel|MongoDB|MySQL
+	public function init()
 	{
-		if ($this->driverObj) {
-			return $this->driverObj;
+		if (!empty($this->authDatabase)) {
+			$this->uri .= "{$this->user}:{$this->password}@{$this->host}:{$this->port}/{$this->authDatabase}";
+		} else {
+			$this->uri .= "{$this->user}:{$this->password}@{$this->host}:{$this->port}/{$this->database}";
 		}
-		$this->driver = $params['driver'] ?? $this->driver;
-		$driver = new DBDriverFactory($this->driver, $params);
-		$this->driverObj = $driver->getDriver();
-		$this->driverObj->customize($this);
-		return $this->driverObj;
+		if (!$this->client) {
+			$this->client = new \MongoDB\Client($this->uri);
+		}
 	}
 
-	/**
-	 * Unset the secured data from the object
-	 *
-	 * @return void
-	 */
-	private function unsetSecuredProperties(): void
+	private function unsetSecuredProperties()
 	{
 		unset($this->database, $this->collection, $this->user, $this->password,
 			$this->host, $this->port, $this->uri, $this->client);
+	}
+
+	/**
+	 * @return \MongoDB\Collection
+	 */
+	public function getCollection()
+	{
+		$collection = $this->collection;
+		$database = $this->database;
+		return $this->client->$database->$collection;
+	}
+
+	/**
+	 * Gets the number of documents matching the filter.
+	 *
+	 * @param array|object $filter Query by which to filter documents
+	 * @param array $options Command options
+	 *
+	 * @return integer
+	 * @see CountDocuments::__construct() for supported options
+	 */
+	public function count(array $filter = [], array $options = [])
+	{
+		try {
+			return $this->getCollection()->countDocuments($filter, $options);
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Finds a single document matching the query.
+	 *
+	 * @see http://docs.mongodb.org/manual/core/read-operations-introduction/
+	 *
+	 * @param array|object $filter Query by which to filter documents
+	 * @param array $options Additional options
+	 *
+	 * @return $this
+	 */
+	public function findOne(array $filter, array $options = [])
+	{
+		try {
+			$this->result = $this->getCollection()->findOne($filter, $options);
+//			$this->unsetSecuredProperties();
+			return $this;
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Finds documents matching the query.
+	 *
+	 * @see http://docs.mongodb.org/manual/core/read-operations-introduction/
+	 *
+	 * @param array|object $filter Query by which to filter documents
+	 * @param array $options Additional options
+	 *
+	 * @return $this
+	 */
+	public function find(array $filter, array $options = [])
+	{
+		try {
+			$this->result = $this->getCollection()->find($filter, $options);
+//			$this->unsetSecuredProperties();
+			return $this;
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Inserts one document.
+	 *
+	 * @see http://docs.mongodb.org/manual/reference/command/insert/
+	 *
+	 * @param array|object $document The document to insert
+	 * @param array $options Command options
+	 *
+	 * @return \MongoDB\BSON\OBjectId inserted ID
+	 */
+	public function insertOne(array $document, array $options = [])
+	{
+		try {
+			$result = $this->getCollection()->insertOne($document, $options);
+			return $result->getInsertedId();
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Inserts multiple documents.
+	 *
+	 * @see http://docs.mongodb.org/manual/reference/command/insert/
+	 *
+	 * @param array[]|object[] $documents The documents to insert
+	 * @param array $options Command options
+	 *
+	 * @return int inserted count
+	 */
+	public function insertMany(array $documents, array $options = [])
+	{
+		try {
+			$result = $this->getCollection()->insertMany($documents, $options);
+			return $result->getInsertedCount();
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Updates at most one document matching the filter.
+	 *
+	 * @see http://docs.mongodb.org/manual/reference/command/update/
+	 *
+	 * @param array|object $filter Query by which to filter documents
+	 * @param array|object $update Update to apply to the matched document
+	 * @param array $options Command options
+	 *
+	 * @return int modified count
+	 */
+	public function updateOne($filter, $update, array $options = [])
+	{
+		try {
+			$result = $this->getCollection()->updateOne($filter, $update, $options);
+			return $result->getModifiedCount();
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Updates at most one document matching the filter.
+	 *
+	 * @see http://docs.mongodb.org/manual/reference/command/update/
+	 *
+	 * @param array|object $filter Query by which to filter documents
+	 * @param array|object $update Update to apply to the matched document
+	 * @param array $options Command options
+	 *
+	 * @return int modified count
+	 */
+	public function replaceOne($filter, $update, array $options = [])
+	{
+		try {
+			$options['upsert'] = true;
+			$result = $this->getCollection()->updateOne($filter, $update, $options);
+			return $result->getUpsertedCount() ?: $result->getMatchedCount();
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Updates all documents matching the filter.
+	 *
+	 * @see http://docs.mongodb.org/manual/reference/command/update/
+	 *
+	 * @param array|object $filter Query by which to filter documents
+	 * @param array|object $update Update to apply to the matched documents
+	 * @param array $options Command options
+	 *
+	 * @return UpdateResult
+	 */
+	public function updateMany($filter, $update, array $options = [])
+	{
+		try {
+			$result = $this->getCollection()->updateMany($filter, $update, $options);
+			return $result->getModifiedCount();
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Deletes at most one document matching the filter.
+	 *
+	 * @see http://docs.mongodb.org/manual/reference/command/delete/
+	 *
+	 * @param array|object $filter Query by which to delete documents
+	 * @param array $options Command options
+	 *
+	 * @return int deleted count
+	 */
+	public function deleteOne($filter, array $options = [])
+	{
+		try {
+			$result = $this->getCollection()->deleteOne($filter, $options);
+			return $result->getDeletedCount();
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Deletes all documents matching the filter.
+	 *
+	 * @see http://docs.mongodb.org/manual/reference/command/delete/
+	 *
+	 * @param array|object $filter Query by which to delete documents
+	 * @param array $options Command options
+	 *
+	 * @return int deleted count
+	 */
+	public function deleteMany($filter, array $options = [])
+	{
+		try {
+			$result = $this->getCollection()->deleteMany($filter, $options);
+			return $result->getDeletedCount();
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Executes an aggregation framework pipeline on the collection.
+	 *
+	 * Note: this method's return value depends on the MongoDB server version
+	 * and the "useCursor" option. If "useCursor" is true, a Cursor will be
+	 * returned; otherwise, an ArrayIterator is returned, which wraps the
+	 * "result" array from the command response document.
+	 *
+	 * @param array $pipeline List of pipeline operations
+	 * @param array $options Command options
+	 *
+	 * @return Traversable
+	 * @see Aggregate::__construct() for supported options
+	 *
+	 */
+	public function aggregate(array $pipeline, array $options = [])
+	{
+		try {
+			$result = $this->getCollection()->aggregate($pipeline, $options);
+			return $result;
+		} catch (\Exception $exception) {
+			Response::error(ResponseCode::E500, Trans::t('app', 'Failure on execute query.'), TutFw::getDebugMode($exception));
+		}
+	}
+
+	/**
+	 * Get returned data after executed query
+	 *
+	 * @return BSONDocument|Cursor
+	 */
+	public function getResult()
+	{
+		return $this->result;
 	}
 }
